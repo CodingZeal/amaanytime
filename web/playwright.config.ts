44136fa75a1578d3016fcf98d36ff1a60f0e5b66
Ryan <github@pocketcereal.com>
2023-01-20 13:22:06 -0800

    Refactor/docker playwright (#17)
    * revert back to sync with main
    
    * sync lock
    
    * codeql ignore seedfile
    
    * codeql ignore
    
    * codeql ignore
    
    * codeql ignore
    
    * codeql ignore again
    
    * remove codeql
    
    * Adjust workflows, fix codeql (#18)
    
    * test codeql
    
    * tweak workflows to follow github docs
    
    * sync with upstream
    
    * fix e2e script bug
    
    * add ci flag
    
    * resync dependences
    
    * fix lint

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -1,7 +1,44 @@
-import type { PlaywrightTestConfig } from '@playwright/test'
+import type { PlaywrightTestConfig, Project } from '@playwright/test'
 import { devices } from '@playwright/test'
 
 const LOCAL_WORKERS = 1
+
+export const BASE_URL = 'http://localhost:8910'
+
+const AVAIL_BROWSERS = {
+  firefox: {
+    name: 'firefox',
+    use: {
+      ...devices['Desktop Firefox'],
+    },
+  },
+  chrome: {
+    name: 'chrome',
+    use: {
+      ...devices['Desktop Chrome'],
+    },
+  },
+  webkit: {
+    name: 'webkit',
+    use: {
+      ...devices['Desktop Safari'],
+    },
+  },
+}
+
+const BROWSERS_TO_RUN = process.env.BROWSERS?.split(',')
+  .map((arg) => {
+    if (arg === '--firefox') {
+      return AVAIL_BROWSERS.firefox
+    }
+    if (arg === '--chrome') {
+      return AVAIL_BROWSERS.chrome
+    }
+    if (arg === '--webkit') {
+      return AVAIL_BROWSERS.webkit
+    }
+  })
+  .filter(Boolean) as Project[]
 
 const config: PlaywrightTestConfig = {
   globalSetup: require.resolve('./playwright.setup.ts'),
@@ -15,31 +52,16 @@ const config: PlaywrightTestConfig = {
   use: {
     actionTimeout: 15 * 1000,
     trace: 'on-first-retry',
+    baseURL: BASE_URL,
   },
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
-  ],
+  projects: BROWSERS_TO_RUN?.length
+    ? BROWSERS_TO_RUN
+    : Object.entries(AVAIL_BROWSERS).map(([_, project]) => project),
   webServer: {
-    reuseExistingServer: true,
     command: 'yarn rw serve',
     port: 8910,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
     env: {
       DISABLE_EMAIL: 'true',
     },
--- web/playwright.setup.ts
+++ web/playwright.setup.ts
@@ -1,10 +1,11 @@
 import { chromium } from '@playwright/test'
 
+import { BASE_URL } from './playwright.config'
+
 async function globalSetup() {
-  const URL = 'http://localhost:8910'
   const browser = await chromium.launch()
   const adminLogin = await browser.newPage()
-  await adminLogin.goto(`${URL}/login`)
+  await adminLogin.goto(`${BASE_URL}/login`)
 
   const usernameInput = adminLogin.getByLabel('username')
   await usernameInput.click()
@@ -15,21 +16,21 @@ async function globalSetup() {
   await passwordInput.fill('password')
 
   await adminLogin.getByRole('button', { name: 'Login' }).click()
-  await adminLogin.waitForURL(URL, { waitUntil: 'domcontentloaded' })
+  await adminLogin.waitForURL(BASE_URL, { waitUntil: 'domcontentloaded' })
 
   await adminLogin
     .context()
     .storageState({ path: 'web/tests/storage/adminUser-pw.json' })
 
   const userLogin = await browser.newPage()
-  await userLogin.goto(`${URL}/login`)
+  await userLogin.goto(`${BASE_URL}/login`)
 
   await userLogin.locator('input[name="username"]').click()
   await userLogin.locator('input[name="username"]').fill('user')
   await userLogin.locator('input[name="password"]').click()
   await userLogin.locator('input[name="password"]').fill('password')
   await userLogin.getByRole('button', { name: 'Login' }).click()
-  await userLogin.waitForURL(URL, { waitUntil: 'domcontentloaded' })
+  await userLogin.waitForURL(BASE_URL, { waitUntil: 'domcontentloaded' })
 
   await userLogin
     .context()
