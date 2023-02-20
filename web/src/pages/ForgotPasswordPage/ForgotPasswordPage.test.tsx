3dd99732930a8194a592097665d9e097f2215b04
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2023-02-20 15:49:10 -0500

    ✨ Forgot Password Styling (#27)
    * Add comments and import env vars
    
    * Stub out basic styles
    
    * Username to email change
    
    * Forgot Password rebased styling
    
    * PR change
    
    * Remove code piece
    
    ---------
    
    Co-authored-by: Sunjay Armstead <connect@sunjayarmstead.local>
    Co-authored-by: natalia <natdiaz2001@gmail.com>

--- web/src/pages/ForgotPasswordPage/ForgotPasswordPage.test.tsx
+++ web/src/pages/ForgotPasswordPage/ForgotPasswordPage.test.tsx
@@ -1,8 +1,27 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen } from '@redwoodjs/testing/web'
 
 import ForgotPasswordPage from './ForgotPasswordPage'
 
+jest.mock('src/hooks/useDevice', () => ({
+  useDevice: jest
+    .fn()
+    .mockImplementationOnce(() => ({ isMobile: true }))
+    .mockImplementation(() => ({ isDesktop: true })),
+}))
+
 describe('ForgotPasswordPage', () => {
+  it('displays mobile', async () => {
+    render(<ForgotPasswordPage />)
+    const login = screen.getByRole('form')
+    expect(login).not.toHaveTextContent('Forgot Password')
+  })
+
+  it('displays desktop', async () => {
+    render(<ForgotPasswordPage />)
+    const login = screen.getByRole('form')
+    expect(login).toHaveTextContent('Forgot Password')
+  })
+
   it('renders successfully', () => {
     expect(() => {
       render(<ForgotPasswordPage />)
