3b39bf2fdeb82e36a5c14c962835bb13534ad3a6
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2023-02-20 15:49:10 -0500

    ✨ Forgot Password Styling (#27)
    * Add comments and import env vars
    
    * Stub out basic styles
    
    * Username to email change
    
    * Forgot Password rebased styling
    
    * PR change
    
    * Remove code piece
    
    ---------
    
    Co-authored-by: Sunjay Armstead <connect@sunjayarmstead.local>
    Co-authored-by: natalia <natdiaz2001@gmail.com>

--- web/src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ web/src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -1,11 +1,15 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
-import { Form, Label, TextField, Submit, FieldError } from '@redwoodjs/forms'
 import { navigate, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 
 import { useAuth } from 'src/auth'
+import { AuthFormWrapper, AuthHeader } from 'src/components/Forms'
+import { ActionButton } from 'src/components/Forms/ActionButton'
+import { TextInput } from 'src/components/Forms/TextInputs/TextInputs'
+import { useDevice } from 'src/hooks/useDevice'
+import { AuthLayout } from 'src/layouts'
 
 const ForgotPasswordPage = () => {
   const { isAuthenticated, forgotPassword } = useAuth()
@@ -16,20 +20,12 @@ const ForgotPasswordPage = () => {
     }
   }, [isAuthenticated])
 
-  const usernameRef = useRef<HTMLInputElement>()
-  useEffect(() => {
-    usernameRef.current.focus()
-  }, [])
-
   const onSubmit = async (data) => {
-    const response = await forgotPassword(data.username)
+    const response = await forgotPassword(data.email)
 
     if (response.error) {
       toast.error(response.error)
     } else {
-      // The function `forgotPassword.handler` in api/src/functions/auth.js has
-      // been invoked, let the user know how to get the link to reset their
-      // password (sent in email, perhaps?)
       toast.success(
         'A link to reset your password was sent to ' + response.email
       )
@@ -37,63 +33,74 @@ const ForgotPasswordPage = () => {
     }
   }
 
+  const { isIPad, isMobile } = useDevice()
   return (
     <>
-      <MetaTags title="Forgot Password" />
-
-      <main className="rw-main">
-        <div className="rw-scaffold rw-login-container">
-          <div className="rw-segment">
-            <header className="rw-segment-header">
-              <h2
-                data-testid="forgot-password-heading"
-                className="rw-heading rw-heading-secondary"
-              >
-                Forgot Password
-              </h2>
-            </header>
-
-            <div className="rw-segment-main">
-              <div className="rw-form-wrapper">
-                <Form onSubmit={onSubmit} className="rw-form-wrapper">
-                  <div className="text-left">
-                    <Label
-                      name="username"
-                      className="rw-label"
-                      errorClassName="rw-label rw-label-error"
-                    >
-                      Username
-                    </Label>
-                    <TextField
-                      data-testid="forgot-password-username"
-                      name="username"
-                      className="rw-input"
-                      errorClassName="rw-input rw-input-error"
-                      ref={usernameRef}
-                      validation={{
-                        required: true,
-                      }}
-                    />
-
-                    <FieldError name="username" className="rw-field-error" />
-                  </div>
-
-                  <div className="rw-button-group">
-                    <Submit
-                      data-testid="forgot-password-submit"
-                      className="rw-button rw-button-blue"
-                    >
-                      Submit
-                    </Submit>
-                  </div>
-                </Form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+      <AuthLayout>
+        <MetaTags title="Forgot Password" />
+        <div className="hidden lg:block"></div>
+        {isMobile || isIPad ? (
+          <MobileForgotPasswordForm onSubmit={onSubmit} isIPad={isIPad} />
+        ) : (
+          <DesktopForgotPasswordForm onSubmit={onSubmit} />
+        )}
+      </AuthLayout>
     </>
   )
 }
+
+const MobileForgotPasswordForm = ({
+  onSubmit,
+  isIPad,
+}: {
+  onSubmit: (data: unknown) => void
+  isIPad
+}) => (
+  <div
+    className={`flex flex-col items-center justify-center ${
+      isIPad
+        ? 'w-[80%] translate-y-[100px] justify-self-center'
+        : 'translate-y-[-35px]'
+    }`}
+  >
+    <AuthHeader>Forgot Password</AuthHeader>
+    <AuthFormWrapper handleSubmit={onSubmit}>
+      <TextInput name="email" label="email" focus />
+      <ActionButton
+        type="submit"
+        style={{
+          marginTop: 20,
+        }}
+      >
+        Submit
+      </ActionButton>
+    </AuthFormWrapper>
+  </div>
+)
+
+const DesktopForgotPasswordForm = ({
+  onSubmit,
+}: {
+  onSubmit: (data: unknown) => void
+}) => (
+  <div>
+    <AuthFormWrapper handleSubmit={onSubmit}>
+      <AuthHeader>Forgot Password</AuthHeader>
+      <TextInput
+        focus
+        name="email"
+        label="Email"
+        labelProps={{
+          className: 'text-sm',
+        }}
+      />
+      <div className="flex max-w-[500px] items-center justify-between py-3">
+        <ActionButton data-testid="login-submit-button" type="submit">
+          Submit
+        </ActionButton>
+      </div>
+    </AuthFormWrapper>
+  </div>
+)
 
 export default ForgotPasswordPage
